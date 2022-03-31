0a642d8b944a0c1c6d5424344b5961bde65d25ee
Sergio Machado Morais Júnior <sergio.smmjr@gmail.com>
2022-03-31 06:48:46 -0300

    Packages and Variables in Go

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Aprendendo Go")
+	name := "John"
+	age := 32
+	version := 1.18
+	fmt.Println("Hello, Sir", name, "you are", age, "years old")
+	fmt.Println("This Program is on the version", version)
+
+	fmt.Println("The type of the variable name is", reflect.TypeOf(version))
 }
