336ad818ff2de0bb1985f837a0eff836aa289973
Sergio Machado Morais Júnior <sergio.smmjr@gmail.com>
2022-04-01 06:42:37 -0300

    Println and ScanF

--- hello.go
+++ hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
@@ -12,5 +11,13 @@ func main() {
 	fmt.Println("Hello, Sir", name, "you are", age, "years old")
 	fmt.Println("This Program is on the version", version)
 
-	fmt.Println("The type of the variable name is", reflect.TypeOf(version))
+	fmt.Println("1 - Initiate Monitoring")
+	fmt.Println("2 - Show Logs")
+	fmt.Println("0 - Exit Program")
+
+	var command int
+	fmt.Scan(&command)
+	fmt.Scanf("%d", &command)
+	fmt.Println("The chosen command was", command)
+
 }
